45195add2517da2329fe849c4623291ee71238b8
natalynyu <natalyn.yu@gmail.com>
2019-03-11 22:47:20 -0400

    add messages for success and failure
    add messages to indicate success and failures for creating an itinerary and viewing an itinerary

---
+++ src/sections/messages.js
@@ -0,0 +1,6 @@
+export default {
+  createItinerarySuccess: 'Successfully created an itinerary!',
+  createItineraryFailure: 'Failed to create itinerary. Please try again.',
+  viewItinerariesSuccess: 'Successfully retrieved all itineraries.',
+  viewItinerariesFailure: 'Failed to retrieve itineraries. Please try again.'
+}
