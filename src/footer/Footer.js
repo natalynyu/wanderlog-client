61d8ece39fefa681411f28f419cc8d3f9fe1350d
natalynyu <natalyn.yu@gmail.com>
2019-03-13 10:47:26 -0400

    add footer to page
    include page footer

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './Footer.scss'
 
 const Footer = () => (
-  <h3>This is my footer</h3>
+  <h5 className="footer">Created by Natalyn Yu - 2019</h5>
 )
 
 export default Footer
